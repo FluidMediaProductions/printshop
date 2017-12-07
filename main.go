3df097bf30db34dd150e226666efb04dff53c085
Benjamin Misell <beninjam174@gmail.com>
2017-12-07 19:49:48 +0000

    Beginings of drawing code

--- main.go
+++ main.go
@@ -1,5 +1,16 @@
 package main
 
+import (
+	"os"
+	_ "image/png"
+	_ "image/jpeg"
+	"image"
+	"image/draw"
+	"image/png"
+)
+
+const base string = "hoodie"
+
 func check(e error) {
 	if e != nil {
 		panic(e)
@@ -41,4 +52,24 @@ func loadBaseImages(name string) (image.Image, image.Image) {
 }
 
 func main() {
+	sourceFile := "source.png"
+	frontImage, backImage := loadBaseImages(base)
+
+	finalImage := image.NewNRGBA(backImage.Bounds())
+
+	if _, err := os.Stat(sourceFile); os.IsNotExist(err) {
+		panic("Source file does not exist")
+	}
+
+	source, err := os.Open(sourceFile)
+	defer source.Close()
+	check(err)
+	sourceImage, _, err := image.Decode(source)
+	check(err)
+
+	draw.Draw(finalImage, finalImage.Bounds(), backImage, image.Pt(0, 0), draw.Over)
+	draw.Draw(finalImage, finalImage.Bounds(), sourceImage, image.Pt(0, 0), draw.Over)
+	draw.Draw(finalImage, finalImage.Bounds(), frontImage, image.Pt(0, 0), draw.Over)
+
+	png.Encode()
 }
