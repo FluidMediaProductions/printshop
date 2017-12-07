5e89b2e09087876068accd00114212bb19b70b1e
Benjamin Misell <beninjam174@gmail.com>
2017-12-07 19:49:05 +0000

    Function to load base images

--- main.go
+++ main.go
@@ -5,5 +5,40 @@ func check(e error) {
 		panic(e)
 	}
 }
+
+func loadBaseImages(name string) (image.Image, image.Image) {
+	backFile := "imgs/"+name+"-b.png"
+	frontFile := "imgs/"+name+"-f.png"
+	frontExists := true
+
+	if _, err := os.Stat(backFile); os.IsNotExist(err) {
+		panic("Base file does not exist")
+	}
+	if _, err := os.Stat(frontFile); os.IsNotExist(err) {
+		frontExists = false
+	}
+
+	back, err := os.Open(backFile)
+	defer back.Close()
+	check(err)
+	backImage, _, err := image.Decode(back)
+	check(err)
+	var frontImage image.Image
+	if frontExists {
+		front, err := os.Open(frontFile)
+		defer front.Close()
+		check(err)
+		frontImage, _, err := image.Decode(front)
+		check(err)
+		if frontImage.Bounds() != backImage.Bounds() {
+			panic("Front and back images are not the same size")
+		}
+	} else {
+		frontImage = image.NewNRGBA(backImage.Bounds())
+	}
+
+	return backImage, frontImage
+}
+
 func main() {
 }
